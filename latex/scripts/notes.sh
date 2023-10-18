efc72b39ca3a7a9716761e2ee3059b1bc836512c
andrew <andrewliu278@gmail.com>
2023-10-17 23:45:49 -0400

    [latexmk, zathura] asy support in latexmk and zathura configs

--- latex/scripts/notes.sh
+++ latex/scripts/notes.sh
@@ -7,4 +7,5 @@ cd "$1"
 mkdir notes
 cp -v -r $DOTFILES/latex/templates/notes/ "$1/notes"
 cp -v $DOTFILES/latex/templates/{_andrew.sty,notes.tex} "$1"
+cp -v $DOTFILES/.config/latexmk/.latexmkrc "$1"
 mv notes.tex main.tex
