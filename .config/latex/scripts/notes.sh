afc84c6d6926437d3a14bc1fc98097f47d117fd2
Andrew Liu <andrewliu278@gmail.com>
2024-05-15 00:03:02 -0400

    [latex] move latex into .config, modify scripts, remove asy

--- .config/latex/scripts/notes.sh
+++ .config/latex/scripts/notes.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 1 ]; then
 fi
 cd "$1"
 mkdir notes
-cp -v -r $DOTFILES/latex/templates/notes/ "$1/notes"
-cp -v $DOTFILES/latex/templates/{_andrew.sty,notes.tex} "$1"
+cp -v -r $DOTFILES/.config/latex/templates/notes/ "$1/notes"
+cp -v $DOTFILES/.config/latex/templates/notes.tex "$1"
 cp -v $DOTFILES/.config/latexmk/.latexmkrc "$1"
 mv notes.tex main.tex
--- .config/latex/scripts/pset.sh
+++ .config/latex/scripts/pset.sh
@@ -3,7 +3,7 @@ if [ "$#" -ne 1 ]; then
 	echo "usage: pset_template <dir>"
 	exit 1
 fi
-cp -v $DOTFILES/latex/templates/{_andrew.sty,pset.tex} "$1"
+cp -v $DOTFILES/.config/latex/templates/pset.tex "$1"
 cp -v $DOTFILES/.config/latexmk/.latexmkrc "$1"
 cd "$1"
 mv pset.tex main.tex
